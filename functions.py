70520341add3e733fb2285391b00e0e3d3dfd244
luis-israel-velez <luis.israel.velez@gmail.com>
2019-01-09 15:30:28 -0800

    New functions

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
-
+import numpy as np
+import math
 
 
 def test():
@@ -13,8 +14,21 @@ def getStockDataVec(key, ftype):
 
     
 	for line in lines[1:]:
-		print("Hello " + line)
 		vec.append(float(line.split(",")[4]))
 
 	return vec
 
+# returns the sigmoid
+def sigmoid(x):
+	return 1 / (1 + math.exp(-x))
+
+def getState(data, t, n):
+	d = t - n + 1
+
+	block = data[d:t + 1] if d >= 0 else -d * [data[0]] + data[0:t + 1] #pad with t0
+	res = []
+
+	for i in range(n - 1):
+		res.append(sigmoid(block[i + 1] - block[i]))
+
+	return np.array([res])
